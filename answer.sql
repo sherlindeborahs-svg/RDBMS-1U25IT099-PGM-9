199c560322017207077cef15cb165c20be6ce334
SHERLIN DEBORAH S <sherlindeborahs@gmail.com>
2026-09-17 11:27:18 +0530

    Program 9

--- answer.sql
+++ answer.sql
@@ -1,1 +1,31 @@
+CREATE DATABASE IF NOT EXISTS CollegeDB;
+USE CollegeDB;
 
+DROP TABLE IF EXISTS Student;
+DROP TABLE IF EXISTS Department;
+
+CREATE TABLE Department (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(50)
+);
+
+INSERT INTO Department (DepartmentID, DepartmentName)
+VALUES
+    (101, 'Computer Science'),
+    (102, 'Mathematics'),
+    (103, 'Physics');
+
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(50),
+    DepartmentID INT,
+    FOREIGN KEY (DepartmentID)
+        REFERENCES Department(DepartmentID)
+);
+
+INSERT INTO Student (StudentID, StudentName, DepartmentID)
+VALUES
+    (1001, 'Arun', 101),
+    (1002, 'Divya', 102),
+    (1003, 'Karthik', 101),
+    (1004, 'Nisha', 103);
